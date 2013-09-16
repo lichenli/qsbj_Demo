28f9db10d3881bac80d12303037aa64e5b27aadf
lichen <937254544@qq.com>
2013-09-16 09:52:23 +0800

    Initial Commit

---
+++ 糗事百科_Demo/LCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCAppDelegate.h
+//  糗事百科_Demo
+//
+//  Created by lichen on 13-9-16.
+//  Copyright (c) 2013年 Lichen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 糗事百科_Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  糗事百科_Demo
+//
+//  Created by lichen on 13-9-16.
+//  Copyright (c) 2013年 Lichen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+    }
+}
